d6576979b146b8715d1f33a8e6ec53e94b96d6a7
BlackCode777 <BlackCode77.1980@gmail.com>
2023-01-25 22:50:49 -0300

    configurando exibição de imagens

--- estudos_com_angular/src/app/home/home.component.ts
+++ estudos_com_angular/src/app/home/home.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { OfertasService } from '../services/ofertas.service';
+import { Oferta } from '../models/oferta.model';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  providers:[OfertasService]
 })
+
 export class HomeComponent {
+
+        public ofertaModel!: Oferta[]; 
+
+        constructor(
+                private ofertasService: OfertasService
+        ){}
+
+        ngOnInit(){
+                this.ofertaModel = this.ofertasService.getOfertas()
+        }
 
 }
