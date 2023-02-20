ea526e334f104748d36097545424895ba0be7bcf
BlackCode777 <BlackCode77.1980@gmail.com>
2023-02-20 20:57:27 -0300

    até aqui consegui fazer a passagem de referência para tag input capturar o objeto da tag select

--- estudos_com_angular/src/app/dropdown-list/dropdown-list.component.ts
+++ estudos_com_angular/src/app/dropdown-list/dropdown-list.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Food } from './IFood.model';
 
 @Component({
   selector: 'app-dropdown-list',
@@ -7,69 +8,42 @@ import { Component } from '@angular/core';
 })
 export class DropdownListComponent {
 
-        /*
-        
-        [placeholder]=" 'Selecione o item' "
-        [settings]="dropdownSettings"
-        [data]="dropdownList"
-        (onSelect)="onItemSelect($event)"
-        (onDeSelect)="onItemDeSelect()"
-        [(ngModel)]="selectedItems"
-        
-        */
-        
-dropdownSettings: any;
-dropdownList: any;
-selectedItems = []
-        
-onItemSelect($event: Event) {
-        
-        let data = this.getData()
-        let selectedItem = data.filter( item => item.item_id == $event)
-        let selectedItemGroup = selectedItem[0]['group']
-        this.dropdownList = data.map(
-                item => {
-                        if( item.group == selectedItemGroup){
-                                item.isDisabled = false
-                        } else {
-                                item.isDisabled = true
-                        }
+                //@Input() model;
+                foodsNgModel = []
+                foods: any = [
+                        {id: '', name: 'Selecione'},
+                        {id: '0', name: 'Steak'},
+                        {id: '1', name: 'Pizza'},
+                        {id: '2', name: 'Tacos'},
+                ];
 
-                        return item
+                // [object HTMLSelectElement]
+
+                funct(){
+                        let valorDoForEach = this.foods.forEach( (valor: { name: string; }) => {
+                                console.log(valor.name)
+                        })
+                        console.log("Aqui")
+                        return valorDoForEach
                 }
-        )
-}
+               
 
-onItemDeSelect() {
+        /**
+         * (method) Array<Food>.forEach(callbackfn: (value: Food, index: number, array: Food[]) => void, thisArg?: any): void
+         */
         
-        if( this.selectedItems && this.selectedItems.length == 0){
-                this.dropdownList = this.dropdownList.map((item: { isDisabled: boolean; }) => {
-                        item.isDisabled = false
-                        return item
-                })
+
+        variavelOuvida: string = ''
+        variavelValueSalve: any ;
+        itemDropDownListen: any = "";
+
+
+                
+       constructor( ){}
+
+        ngOnInit(){ 
+                this.funct()
         }
-}
 
-constructor( ){}
-
-getData(): Array<any> {
-        return[
-                { item_id: 1,   item_tex:"Apple",   group:"F" },
-                { item_id: 2,   item_tex:"Orange",   group:"F" },
-                { item_id: 3,   item_tex:"Potatoes",   group:"F" },
-                { item_id: 4,   item_tex:"Cabbage",   group:"V" },
-                { item_id: 5,   item_tex:"Cauliflower",   group:"V" }
-        ]
-}
-
-ngOnInit(){
-        
-        this.dropdownList = this.getData()
-
-}
-
-
-classificandoParametrosDaFuncao(){
-}
 
 }
