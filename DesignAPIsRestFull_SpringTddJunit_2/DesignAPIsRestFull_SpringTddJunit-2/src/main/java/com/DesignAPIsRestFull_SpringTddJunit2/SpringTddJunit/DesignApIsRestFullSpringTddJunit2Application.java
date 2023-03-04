6d5b544a466e04cb77e856f7ba8b10dd512f54a2
BlackCode777 <BlackCode77.1980@gmail.com>
2023-03-04 13:20:48 -0300

    version_1 - init project - DesignAPIsRestFull_SpringTddJunit_2

---
+++ DesignAPIsRestFull_SpringTddJunit_2/DesignAPIsRestFull_SpringTddJunit-2/src/main/java/com/DesignAPIsRestFull_SpringTddJunit2/SpringTddJunit/DesignApIsRestFullSpringTddJunit2Application.java
@@ -0,0 +1,13 @@
+package com.DesignAPIsRestFull_SpringTddJunit2.SpringTddJunit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignApIsRestFullSpringTddJunit2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesignApIsRestFullSpringTddJunit2Application.class, args);
+	}
+
+}
---
+++ DesignAPIsRestFull_SpringTddJunit_2/DesignAPIsRestFull_SpringTddJunit-2/src/test/java/com/DesignAPIsRestFull_SpringTddJunit2/SpringTddJunit/DesignApIsRestFullSpringTddJunit2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DesignAPIsRestFull_SpringTddJunit2.SpringTddJunit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesignApIsRestFullSpringTddJunit2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
