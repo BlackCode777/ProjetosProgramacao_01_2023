8f385aeea9c03b77d9c0ebf8e7a239a65d3c0dce
BlackCode777 <blackcode77.1980@gmail.com>
2023-04-09 01:13:32 -0300

    Treinamento de APIs rest com spring boot

---
+++ conceitos_java_2/curso-spring-dev/src/main/java/br/com/springboot/curso_spring_dev/controllers/GreetingsController.java
@@ -0,0 +1,114 @@
+package br.com.springboot.curso_spring_dev.controllers;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.springboot.curso_spring_dev.model.Usuario;
+import br.com.springboot.curso_spring_dev.repository.UsuarioRepository;
+
+/**
+ *
+ * A sample greetings controller to return greeting text
+ */
+@RestController
+public class GreetingsController {
+
+	@Autowired 
+	private UsuarioRepository usuarioRepository;	
+	
+    @RequestMapping(value = "/{name}", method = RequestMethod.GET)
+    @ResponseStatus(HttpStatus.OK)
+    public String greetingText(@PathVariable String name) {
+        return "Hello " + name + "!";
+    }    
+    @RequestMapping( value = "/olaMundo/{nome}", method = RequestMethod.GET )
+    @ResponseStatus( HttpStatus.OK )
+    public String metodoRetorno2( @PathVariable String nome ) {
+		return "Ola mundo " + nome;    	
+    }
+    
+    @GetMapping( value = "listaTodos" )
+    @ResponseBody//Retorna os dados para o corpo da resposta
+    public ResponseEntity<List<Usuario>>  getUsuario() {
+		List<Usuario> usuarios = usuarioRepository.findAll();
+		return new ResponseEntity<List<Usuario>>(usuarios, HttpStatus.OK);//retorna a lista em json
+	}
+    
+    @PostMapping( value = "salvar" )
+    @ResponseBody
+    public ResponseEntity<Usuario> salvar( @RequestBody Usuario usuario ){
+    	Usuario user = usuarioRepository.save(usuario);
+    	return new ResponseEntity<Usuario>( user, HttpStatus.CREATED );
+    }
+    
+    @DeleteMapping( value = "delete" )
+    @ResponseBody
+    public ResponseEntity<String> deletar( @RequestParam Long iduser ){
+    	usuarioRepository.deleteById(iduser);;
+    	return new ResponseEntity<String>( "Usuario deletado com sucesso !  ", HttpStatus.OK );
+    }
+    
+    @GetMapping( value = "buscarUserId" )
+    @ResponseBody
+    public ResponseEntity<Usuario> buscarUserId( @RequestParam( name = "iduser" ) Long iduser ){
+    	Usuario user = usuarioRepository.findById(iduser).get();
+    	return new ResponseEntity<Usuario>( user, HttpStatus.OK );
+    }
+    
+    @PutMapping( value = "atualizar" )
+    @ResponseBody
+    public ResponseEntity<?> atualizar( @RequestBody Usuario usuario ){
+    	
+    	if( usuario.getId() == null ) {
+    		return new ResponseEntity<String>( " Id do usuário não foi informado. ", HttpStatus.BAD_REQUEST );
+    	}    
+    	
+    	Usuario user = usuarioRepository.saveAndFlush(usuario);
+    	return new ResponseEntity<Usuario>( user, HttpStatus.OK );
+    }
+    
+    //consultarPorNome
+    @GetMapping( value = "buscarPorNome" )
+    @ResponseBody // buscar por nomes - retirando espaços em branco - e filtrando caracteres maiúsculos e minúsculos
+    public ResponseEntity<List<Usuario>> buscarPorNome( @RequestParam( name = "nome" ) String nome ){
+    	List<Usuario> nomeUser = usuarioRepository.findByName( nome.trim().toUpperCase() ); 
+    	return new ResponseEntity<List<Usuario>>( nomeUser, HttpStatus.OK );
+    }
+    
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
